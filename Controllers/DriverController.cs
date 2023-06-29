2638f7196bb3d62e381ea73c5f7e46a3120b50c4
maicol hernandez <maykolandres4@gmail.com>
2023-06-29 04:18:48 -0500

    created controller

---
+++ Controllers/DriverController.cs
@@ -0,0 +1,42 @@
+﻿using DNC_API.Data;
+using Microsoft.AspNetCore.Mvc;
+using DNC_API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DNC_API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class DriverController : ControllerBase
+    {
+
+        private readonly ILogger<DriverController> _logger;
+
+        private readonly ApiDbContext _context;
+
+        public DriverController( ILogger<DriverController> logger, ApiDbContext context )
+        {
+            _logger = logger;
+            _context = context;
+           
+        }
+
+        [HttpGet(Name = "GetAllDrivers")]
+
+        public async Task<IActionResult> Get()
+        {
+            var driver = new Driver()
+            {
+                DriverNumber = 44,
+                Name = "sir leis"
+            };
+
+            _context.Add(driver);
+            await _context.SaveChangesAsync();
+
+            var allDrivers = await _context.Drivers.ToListAsync();
+
+            return Ok(allDrivers);
+        }
+    }
+}
