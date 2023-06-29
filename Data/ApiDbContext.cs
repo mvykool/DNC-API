2b9d4766300b7a72e192a228071522a305f5aca5
maicol hernandez <maykolandres4@gmail.com>
2023-06-29 04:00:45 -0500

    Add project files.

---
+++ Data/ApiDbContext.cs
@@ -0,0 +1,15 @@
+﻿using DNC_API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DNC_API.Data
+{
+    public class ApiDbContext : DbContext
+    {
+        public ApiDbContext(DbContextOptions<ApiDbContext> options) : base (options)
+        {
+            
+        }
+
+        public DbSet<Driver> Drivers { get; set; }
+    }
+}
---
+++ Models/Driver.cs
@@ -0,0 +1,11 @@
+﻿namespace DNC_API.Models
+{
+    public class Driver
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; } = null;
+
+        public int DriverNumber { get; set; }
+    }
+}
